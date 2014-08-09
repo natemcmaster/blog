6ecae813ca7566636d5bdfbf23819b1de4e8e8b2
Nate McMaster <nate.mcmaster@gmail.com>
2014-08-09 12:04:21 -0600

    update ga

--- src/_assets/javascripts/ga.js
+++ src/_assets/javascripts/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-38984092-1', 'natemcmaster.com');
-ga('send','pageview');
+ga('create', 'UA-38984092-1', 'auto');
+ga('send', 'pageview');
