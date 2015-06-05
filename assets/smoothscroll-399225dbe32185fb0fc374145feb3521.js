a1e3d6e9d442b2e537b8d0c34341e3d3353d415e
Nate McMaster <nate.mcmaster@gmail.com>
2015-06-04 20:51:40 -0700

    Build Thu Jun  4 20:51:40 PDT 2015

--- assets/smoothscroll-399225dbe32185fb0fc374145feb3521.js
+++
@@ -1,1 +0,0 @@
-$(function(){$("a[href*=#]:not([href=#])").click(function(){if(location.pathname.replace(/^\//,"")==this.pathname.replace(/^\//,"")&&location.hostname==this.hostname){var target=$(this.hash);if(target=target.length?target:$("[name="+this.hash.slice(1)+"]"),target.length)return $("html,body").animate({scrollTop:target.offset().top},500),!1}})});
