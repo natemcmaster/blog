c6733a47187ade1010996f5bfbc1d2a886ba153d
Nate McMaster <nate.mcmaster@gmail.com>
2017-03-17 23:25:31 -0700

    Add section anchor links

---
+++ assets/scripts/site.js
@@ -0,0 +1,11 @@
+$(function() {
+  return $(".post-content h1, h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="icon-link"></i>';
+    if (id) {
+      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
